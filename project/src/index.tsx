06b9a9f34c52adb4ae229af15ef356c0cc14dfa4
Kirill Malskiy <malskykirill@yandex.ru>
2022-07-09 12:46:04 +0300

    2.9. Начинаем программировать

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+// const Rating = {
+//   STARS_COUNT: 4.5,
+// };
+
+const Place = {
+  PLACES_COUNT: 100,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      placesCount={Place.PLACES_COUNT}
+    />
   </React.StrictMode>,
 );
